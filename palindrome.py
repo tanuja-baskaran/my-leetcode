1c94dd571fa9264053dd0bca5bebfaabd7114e54
tanuja-baskaran <tanujabhaskaran@gmail.com>
2025-10-08 18:51:43 +0530

    palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isPalindrome(self, x):
+  
+        if x < 0:
+            return False
+        
+        rev = 0
+        num = x
+        
+        while num != 0:
+            rev = rev * 10 + num % 10
+            num = num // 10
+        
+        return rev == x
+        
